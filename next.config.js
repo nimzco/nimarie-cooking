2cee3b62fdf701029ae191845d23f5938087278e
Nima Izadi <nim.izadi@gmail.com>
2019-03-08 12:02:47 -0500

    Converted everything to TS

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 const withSass = require('@zeit/next-sass');
-// const withTypescript = require('@zeit/next-typescript');
+const withTypescript = require('@zeit/next-typescript');
 
-// module.exports = withTypescript();
-module.exports = withSass({
+module.exports = withSass(withTypescript({
+  target: 'serverless',
   cssModules: true,
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: "[local]___[hash:base64:5]",
-  }
-});
+  },
+}));
